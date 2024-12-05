a649f867895750de8b9a7ecade77640333d1e576
Jiani Yu <you@example.com>
2024-12-04 17:03:53 -0800

    again

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'docs',  // This will output to /docs
+  basePath: '/income-visualization'
 };
 
 export default nextConfig;
